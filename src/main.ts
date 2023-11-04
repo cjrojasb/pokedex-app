fbb952f10f28452daa28686ce78c3e336003ccac
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2023-11-03 21:28:31 -0300

    feat: set global api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const API_PREFIX = 'api/v2';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix(API_PREFIX);
   await app.listen(8000);
 }
 bootstrap();
