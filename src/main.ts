3c49e8511ecca73917345b9f2ca8b879ef143c23
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2023-11-06 14:53:24 -0300

    feat: was added mongoose and class vaidators for handle dtos

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const API_PREFIX = 'api/v2';
@@ -7,6 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix(API_PREFIX);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(8000);
 }
 bootstrap();
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePokemonDto {}
+import { IsString, IsPositive, IsNumber } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  @IsPositive()
+  no: number;
+}
