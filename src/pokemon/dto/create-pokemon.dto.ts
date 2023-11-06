927942b10a29007cdc5e66e90a18fb81314c61f2
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2023-11-06 14:58:00 -0300

    feat: add validators to create-pokemon.dto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,10 +1,12 @@
-import { IsString, IsPositive, IsNumber } from 'class-validator';
+import { IsString, IsPositive, IsInt, MinLength, Min } from 'class-validator';
 
 export class CreatePokemonDto {
   @IsString()
+  @MinLength(1)
   name: string;
 
-  @IsNumber()
+  @IsInt()
   @IsPositive()
+  @Min(1)
   no: number;
 }
