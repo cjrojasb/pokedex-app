4a54d89c3db7ce280b205171d3de4b10e8e33774
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2023-11-07 17:37:03 -0300

    feat: add progress in seed

--- src/seed/interfaces/pokemon-response.interface.ts
+++ src/seed/interfaces/pokemon-response.interface.ts
@@ -1,4 +1,4 @@
-export interface Pokemon {
+export interface PokemonResponse {
   count: number;
   next: string;
   previous: null;
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
-import { Pokemon } from './interfaces/pokemon-response.interface';
+import axios, { AxiosInstance } from 'axios';
+import { PokemonResponse } from './interfaces/pokemon-response.interface';
 
 @Injectable()
 export class SeedService {
-  private http = axios;
-  private pokeapiUrl = 'https://pokeapi.co/api/v2/pokemon';
+  private readonly http: AxiosInstance = axios;
+  private pokeapiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=15';
 
   async setSeed() {
-    const { data } = await this.http.get<Pokemon>(this.pokeapiUrl);
+    const { data } = await this.http.get<PokemonResponse>(this.pokeapiUrl);
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = Number(segments[segments.length - 2]);
+
+      console.log({ name, no });
+    });
+
     return data.results;
   }
 }
