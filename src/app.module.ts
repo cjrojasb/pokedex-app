d9618169690ff5844825708441e8713824414b1d
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2023-11-08 18:15:26 -0300

    feat: joi was added for env schma

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 import { CommonModule } from './common/common.module';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     PokemonModule,
     SeedModule,
